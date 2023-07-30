7b6bc6d457591eecdcf86582d1e418f023a2da1f
Marco Roth <marco.roth@intergga.ch>
2023-07-30 22:59:41 +0200

    Rework sidebars

--- app/components/modules_list.rb
+++ app/components/modules_list.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class ModulesList < ViewComponent::Base
-  def initialize(modules:, gem:)
-    @modules = modules.sort_by(&:name)
+  def initialize(modules:, gem:, title: "Modules")
+    @modules = Array.wrap(modules).sort_by(&:name)
     @gem = gem
+    @title = title
   end
 end
