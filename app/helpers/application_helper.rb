015815d219bc1a30093727b530a640d9f3d7e6af
Marco Roth <marco.roth@intergga.ch>
2023-07-31 17:02:20 +0200

    Add `ApplicationHelper#to_short_sentence`

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def to_short_sentence(array, limit: 3)
+    if array.length > limit
+      remaining = array.length - limit
+
+      "#{array.first(3).join(", ")} and #{remaining} more"
+    else
+      array.to_sentence
+    end
+  end
 end
