fbf037ad7985e00c323190ff425f628e32c5aaa1
Marco Roth <marco.roth@intergga.ch>
2023-08-04 22:15:28 +0200

    Don't run RoRvsWild in development

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -1,4 +1,6 @@
-if api_key = Rails.application.credentials.dig(:rorvswild, :api_key)
+return unless Rails.env.production?
+
+if (api_key = Rails.application.credentials.dig(:rorvswild, :api_key))
   RorVsWild.start(
     api_key: api_key,
     ignored_exceptions: [
