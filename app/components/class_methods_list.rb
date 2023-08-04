2fc56f7211809087c3a8fa263b055deaea652d54
Marco Roth <marco.roth@intergga.ch>
2023-08-04 04:28:28 +0200

    Replace `ClassMethodsList` and `InstanceMethodsList` with `MethodsList`

--- app/components/class_methods_list.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ClassMethodsList < ViewComponent::Base
-  def initialize(class_methods:, gem:)
-    @class_methods = class_methods.sort_by(&:name)
-    @gem = gem
-  end
-end
--- app/components/instance_methods_list.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class InstanceMethodsList < ViewComponent::Base
-  def initialize(instance_methods:, gem:)
-    @instance_methods = instance_methods.sort_by(&:name)
-    @gem = gem
-  end
-end
