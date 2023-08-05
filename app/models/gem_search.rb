5db6921845c46c4c8ca015341fa31f70d53c50e3
Marco Roth <marco.roth@intergga.ch>
2023-08-05 18:01:39 +0200

    Refactor `GemSearch::search` using `gems` gem
    Addresses https://github.com/marcoroth/gem.sh/issues/2

--- app/models/gem_search.rb
+++ app/models/gem_search.rb
@@ -1,13 +1,7 @@
 class GemSearch
-  URL = "https://rubygems.org/api/v1/search/autocomplete"
-
   def self.search(query)
     return [] if query.blank?
 
-    url = "#{URL}?query=#{query}"
-
-    result = HTTParty.get(url)
-
-    JSON.parse(result.response.body)
+    Gems.search(query)
   end
 end
