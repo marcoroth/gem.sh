3a83e4c2251de8889ea1bd54ce0cb62abcc12903
Marco Roth <marco.roth@intergga.ch>
2023-08-06 17:36:24 +0200

    Move `default_meta_tags` to `MetaHelper`

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,4 @@
 module ApplicationHelper
-  def default_meta_tags
-    {
-      site: "gem.sh",
-      title: "",
-      reverse: true,
-      separator: "-",
-      description: "Beautiful documentation for any Ruby gem",
-      keywords: "ruby, rails, rubyonrails, programming, documentation",
-      canonical: request.original_url,
-      noindex: !Rails.env.production?,
-      og: {
-        site_name: "gem.sh",
-        title: "gem.sh",
-        description: "Beautiful documentation for any Ruby gem",
-        type: "website",
-        url: request.original_url,
-        image: image_url("social.png")
-      }
-    }
-  end
-
   def to_short_sentence(array, limit: 3)
     if array.length > limit
       remaining = array.length - limit
