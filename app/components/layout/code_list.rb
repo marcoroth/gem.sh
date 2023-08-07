67c45f8a5688160c6761823786adaa4db991b709
Marco Roth <marco.roth@intergga.ch>
2023-08-08 00:37:01 +0200

    Cast `items` in `Layout::CodeList` to `Array` in case it's a `Set`

--- app/components/layout/code_list.rb
+++ app/components/layout/code_list.rb
@@ -2,6 +2,8 @@
 
 class Layout::CodeList < ViewComponent::Base
   def initialize(items:, gem:, title: "Items")
+    items = items.to_a if items.is_a?(Set)
+
     @items = Array.wrap(items).sort_by { |item| item.try(:title) || item.to_s }
     @gem = gem
     @title = title
