5f1075b42e690af0ddb782692a5f04a23dd13b70
Marco Roth <marco.roth@intergga.ch>
2023-08-09 22:04:34 +0200

    Fix typo in `dependencies`

--- app/models/metadata.rb
+++ app/models/metadata.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Metadata = Data.define(:files, :dependenices, :authors, :error) do
+Metadata = Data.define(:files, :dependencies, :authors, :error) do
   def initialize(files: [], dependencies: [], authors: [], error: nil)
     super
   end
