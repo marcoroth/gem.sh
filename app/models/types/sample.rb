e4d568bf92d984767a7a099b66457eab2fe60b02
Marco Roth <marco.roth@intergga.ch>
2023-08-15 00:06:58 +0200

    Add validations for the `Types::Sample` model

--- app/models/types/sample.rb
+++ app/models/types/sample.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Types::Sample < ApplicationRecord
+  validates :gem_name, :gem_version, :method_name, :location, :type_fusion_version, :application_name, :source_ip, :parameters, presence: true
 end
