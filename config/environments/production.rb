bdb0268feb92fa601aef5c9456b0b371bd8ec47c
Marco Roth <marco.roth@intergga.ch>
2023-08-14 17:21:01 +0200

    Run Tests in CI (#40)
    * Run Tests in CI
    
    * Rename app to `Gemsh`

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "web_production"
+  # config.active_job.queue_name_prefix = "gemsh_production"
 
   config.action_mailer.perform_caching = false
 
