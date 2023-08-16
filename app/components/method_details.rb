e00eb361e5a7a86666312a29f149391417a8dd67
Marco Roth <marco.roth@intergga.ch>
2023-08-16 22:47:20 +0200

    Cleanup `MethodDetails` markup

--- app/components/method_details.rb
+++ app/components/method_details.rb
@@ -6,4 +6,24 @@ class MethodDetails < ViewComponent::Base
     @parent = parent
     @gem = gem
   end
+
+  def comments_content
+    @object.comments.join("<br>")
+  end
+
+  def yard
+    YARD::Templates::Helpers::Markup::RDocMarkup.new(comments_content).to_html
+  end
+
+  def rdoc
+    @rdoc ||= RDoc::Markup::ToHtml.new(RDoc::Options.new)
+  end
+
+  def rbs_signature?
+    @object.try(:samples, @gem).present?
+  end
+
+  def rbs_signature
+    @object.rbs_signature(@gem)
+  end
 end
