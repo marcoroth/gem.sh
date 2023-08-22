b21de021c28621ea6d8ded9d48cb9720ef66d529
Marco Roth <marco.roth@intergga.ch>
2023-08-22 19:01:34 +0200

    Add superclass to RBS signature

--- app/models/concerns/rbs_namespace_helpers.rb
+++ app/models/concerns/rbs_namespace_helpers.rb
@@ -6,10 +6,12 @@ module RBSNamespaceHelpers
 
     return nil if rbs_method_signatures.empty?
 
+    rbs_superclass = (defined?(superclass) && superclass) ? " < #{superclass.qualified_name}" : nil
+
     <<~RBS
       # #{defined_files.first.path.gsub("#{gem.unpack_data_path}/lib/", 'sig/')}s
 
-      #{object_name} #{qualified_name}
+      #{object_name} #{qualified_name}#{rbs_superclass}
         #{rbs_method_signatures.map { |sig| sig.gsub("\n", "\n  ") }.join("\n  ")}
       end
     RBS
