bc51f93d8d98c29793776e4f4f348fc5471fe213
Marco Roth <marco.roth@intergga.ch>
2023-08-25 05:45:32 +0200

    RuboCop

--- app/controllers/gems_controller.rb
+++ app/controllers/gems_controller.rb
@@ -18,12 +18,14 @@ class GemsController < ApplicationController
     @gems = Gem::Specification.all.sort_by(&:name)
   end
 
+  Entry = Struct.new(:title, :url, :description, :icon)
+
   def show
     @getting_started = [
-      OpenStruct.new(title: "Installation", url: Router.gem_readme_path(@gem.name, @gem.version), description: "Learn more about how to install and configure the gem", icon: "arrow-down-tray"),
-      OpenStruct.new(title: "Documentation", url: Router.gem_guides_path(@gem.name, @gem.version), description: "Learn more about the details", icon: "book-open"),
-      OpenStruct.new(title: "Guides", url: Router.gem_guides_path(@gem.name, @gem.version), description: "Learn more about the gem in the written guides", icon: "document-text"),
-      OpenStruct.new(title: "Reference", url: Router.gem_reference_path(@gem.name, @gem.version), description: "Learn more about the classes and modules", icon: "code-bracket"),
+      Entry.new(title: "Installation", url: Router.gem_readme_path(@gem.name, @gem.version), description: "Learn more about how to install and configure the gem", icon: "arrow-down-tray"),
+      Entry.new(title: "Documentation", url: Router.gem_guides_path(@gem.name, @gem.version), description: "Learn more about the details", icon: "book-open"),
+      Entry.new(title: "Guides", url: Router.gem_guides_path(@gem.name, @gem.version), description: "Learn more about the gem in the written guides", icon: "document-text"),
+      Entry.new(title: "Reference", url: Router.gem_reference_path(@gem.name, @gem.version), description: "Learn more about the classes and modules", icon: "code-bracket"),
     ]
   end
 
