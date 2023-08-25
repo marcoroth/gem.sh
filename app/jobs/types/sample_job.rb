922674a601258dd19f2a5d9ae660dedb7839dcc6
Marco Roth <marco.roth@intergga.ch>
2023-08-25 16:09:14 +0200

    Retry `Types::SampleJob` if it failed to persit

--- app/jobs/types/sample_job.rb
+++ app/jobs/types/sample_job.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 class Types::SampleJob < ApplicationJob
+  retry_on Exception, wait: :exponentially_longer, attempts: 50
+
   queue_as :default
 
   def perform(sample, source_ip)
-    Rails.logger.debug ::Types::Sample.create(**sample.merge(source_ip:)).inspect
+    sample = ::Types::Sample.create(**sample.merge(source_ip:))
+
+    Rails.logger.debug sample.inspect
+
+    raise "Couldn't process sample" unless sample.persisted?
   end
 end
