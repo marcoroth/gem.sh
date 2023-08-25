9afda1742e1fe4c48616e5551b57b7c561a93160
Marco Roth <marco.roth@intergga.ch>
2023-08-25 16:15:17 +0200

    Setup TypeFusion for sampling in test env

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+require "type_fusion/minitest"
+
 FactoryBot.find_definitions
 
 class ActiveSupport::TestCase
