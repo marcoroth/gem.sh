c454c80d5a77271848c8808ff73455ee44dabfcb
Marco Roth <marco.roth@intergga.ch>
2023-08-28 23:54:43 +0200

    Redirect back to gems path if HTML-requesting search path

--- app/controllers/gems/search_controller.rb
+++ app/controllers/gems/search_controller.rb
@@ -4,10 +4,15 @@ class Gems::SearchController < ApplicationController
   include GemScoped
 
   def index
-    query = params[:q].downcase
+    query = params[:q].to_s.downcase
 
     @modules = @gem.modules.select { |mod| mod.qualified_name.downcase.include?(query) }
     @classes = @gem.classes.select { |klass| klass.qualified_name.downcase.include?(query) }
     @methods = @gem.all_methods.select { |method| method.name.downcase.include?(query) }
+
+    respond_to do |format|
+      format.html { redirect_to gem_version_path(@gem.name, @gem.version) }
+      format.turbo_stream
+    end
   end
 end
