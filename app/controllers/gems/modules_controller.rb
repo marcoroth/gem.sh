555ed007bd637206fc066fd9d58d0da0d61841b1
Kasper Timm Hansen <hey@kaspth.com>
2025-10-20 14:23:11 +0200

    Inline GemModuleScoped; it's only used in one method right now

--- app/controllers/gems/modules_controller.rb
+++ app/controllers/gems/modules_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class Gems::ModulesController < Gems::BaseController
-  include GemModuleScoped
-
   def index
   end
 
   def show
+    @module = @gem.find_module!(params[:id])
+
     @modules = @gem.modules.select { |mod| mod.qualified_namespace == @module.qualified_name }
     @classes = @gem.classes.select { |klass| klass.qualified_namespace == @module.qualified_name }
   end
