9ed0db0c7b710bfbd664eda0ca64206cf9dc2b5f
Marco Roth <marco.roth@intergga.ch>
2025-10-23 22:31:15 +0200

    `ReActionView.intercept_erb = Rails.env.development?`

--- config/initializers/reactionview.rb
+++ config/initializers/reactionview.rb
@@ -2,7 +2,7 @@
 
 ReActionView.configure do |config|
   # Intercept .html.erb templates and process them with `Herb::Engine` for enhanced features
-  config.intercept_erb = true
+  config.intercept_erb = Rails.env.development?
 
   # Enable debug mode in development (adds debug attributes to HTML)
   config.debug_mode = Rails.env.development?
